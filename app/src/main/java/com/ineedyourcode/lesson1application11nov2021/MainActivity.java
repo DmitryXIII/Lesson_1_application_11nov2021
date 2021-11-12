7828f70a5de446b69455cf78825c2a6d5c44ebdf
Dmitry Gusev <dmtrxiii@gmail.com>
2021-11-12 13:03:56 +0300

    Initial commit

---
+++ app/src/main/java/com/ineedyourcode/lesson1application11nov2021/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ineedyourcode.lesson1application11nov2021;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
