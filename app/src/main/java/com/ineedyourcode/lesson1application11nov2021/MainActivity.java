89dacdae4d4a965ddc26e6cc7e8a3cd7ae285083
Dmitry Gusev <dmtrxiii@gmail.com>
2021-11-14 15:40:54 +0300

    layout 1 released

--- app/src/main/java/com/ineedyourcode/lesson1application11nov2021/MainActivity.java
+++ app/src/main/java/com/ineedyourcode/lesson1application11nov2021/MainActivity.java
@@ -1,24 +1,92 @@
 package com.ineedyourcode.lesson1application11nov2021;
 
+/*
+====================================
+EMS - параметр, определяющий максимальный размер TextView. 1 единица EMS равна ширине заглавной латинской буквы 'M'
+в выбранном размере выбранного шрифта. Т.е. при одном и том же шрифте при размерах, например, 12sp и 20sp
+размер 1 единицы EMS будет разным.
+EMS уитывается только при установленном параметре android:layout_width="wrap_content".
+====================================
+*/
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
+import android.view.Gravity;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
+    private static int clickCounter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        LinearLayout activityMain = findViewById(R.id.layout_main);
+        EditText etxtTypeAnything = findViewById(R.id.etxt_type_anything);
+        TextView txtNumberOfChars = findViewById(R.id.txt_number_of_chars);
+        TextView txtCheckBoxStatus = findViewById(R.id.txt_checkbox_status);
+        TextView txtSwitchStatus = findViewById(R.id.txt_switch_status);
+        TextView txtBuildNumber = findViewById(R.id.txt_build_number);
         Button btnChangeLayout1to2 = findViewById(R.id.btn_change_layout_1_to_2);
+        Button btnCalculateChars = findViewById(R.id.btn_calculate_chars);
+        ToggleButton toggleGreenBG = findViewById(R.id.toggle_green_background);
+        CheckBox checkBox = findViewById(R.id.cbx_checkbox_1);
+        Switch switcher = findViewById(R.id.sw_switch_1);
 
         btnChangeLayout1to2.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, SecondLayout.class);
             startActivity(intent);
+        });
+
+        btnCalculateChars.setOnClickListener(v -> txtNumberOfChars.setText("You entered " + etxtTypeAnything.getText().toString().length() + " chars"));
+
+        checkBox.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (buttonView.isChecked()) {
+                txtCheckBoxStatus.setText("CheckBox is checked");
+                switcher.setChecked(false);
+            } else {
+                txtCheckBoxStatus.setText("CheckBox is unchecked");
+                switcher.setChecked(true);
+            }
+        });
+
+        switcher.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (buttonView.isChecked()) {
+                txtSwitchStatus.setText("Switch is checked");
+                checkBox.setChecked(false);
+            } else {
+                txtSwitchStatus.setText("Switch is unchecked");
+                checkBox.setChecked(true);
+            }
+        });
+
+        toggleGreenBG.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (buttonView.isChecked()) {
+                activityMain.setBackgroundColor(Color.parseColor("#B8FFBB"));
+            } else {
+                activityMain.setBackgroundColor(Color.parseColor("#FFFFF8C0"));
+            }
+        });
+
+        txtBuildNumber.setOnClickListener(v -> {
+            clickCounter++;
+            if (clickCounter > 3 && clickCounter < 8) {
+                Toast toastYouAreDeveloper = Toast.makeText(getApplicationContext(), (clickCounter < 7) ? ("You are in " + (7 - clickCounter) + " steps away from being a developer!") : ("You are developer!"), Toast.LENGTH_SHORT);
+                toastYouAreDeveloper.setGravity(Gravity.BOTTOM, 0, 0);
+                toastYouAreDeveloper.show();
+            }
         });
     }
 }
--- app/src/main/java/com/ineedyourcode/lesson1application11nov2021/SecondLayout.java
+++ app/src/main/java/com/ineedyourcode/lesson1application11nov2021/SecondLayout.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,14 +13,31 @@ public class SecondLayout extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_layout);
 
         Button btnChangeLayout2to1 = findViewById(R.id.btn_change_layout_2_to_1);
+        TextView txtBirthDate = findViewById(R.id.txt_birthdate);
+        CalendarView calendar = findViewById(R.id.calendar);
+        calendar.setVisibility(View.GONE);
 
         btnChangeLayout2to1.setOnClickListener(v -> {
             Intent intent = new Intent(SecondLayout.this, MainActivity.class);
             startActivity(intent);
+        });
+
+        txtBirthDate.setOnClickListener(v -> calendar.setVisibility(View.VISIBLE));
+
+        calendar.setOnDateChangeListener((view, year, month, dayOfMonth) -> {
+            int mYear = year;
+            int mMonth = month;
+            int mDay = dayOfMonth;
+            String selectedDate = new StringBuilder().append(String.valueOf(mDay).length() < 2 ? "0" + mDay : mDay).append(".")
+                    .append(String.valueOf(mMonth + 1).length() < 2 ? "0" + (mMonth + 1) : (mMonth + 1)).append(".")
+                    .append(mYear).toString();
+            txtBirthDate.setText(selectedDate);
+            calendar.setVisibility(View.GONE);
         });
     }
 }
